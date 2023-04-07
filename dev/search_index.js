8855cb94862469e35787f6373ebc186b7c3403fb
Documenter.jl <documenter@juliadocs.github.io>
2023-04-07 18:21:03 +0000

    build based on 442e9b0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = jlox","category":"page"},{"location":"#jlox","page":"Home","title":"jlox","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for jlox.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [jlox]","category":"page"}]
+}
